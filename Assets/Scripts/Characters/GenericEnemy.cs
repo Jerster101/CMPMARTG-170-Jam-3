2c4f226eab52fdee7182594e9d139f1768c22cd9
Niccolo Abate <lofieloomis@gmail.com>
2022-11-29 12:43:19 -0800

    Updated enemy targeting / AI
    Targeting lambda set up
    Waiting set up in enemy so player has a chance to see what happens

--- Assets/Scripts/Characters/GenericEnemy.cs
+++ Assets/Scripts/Characters/GenericEnemy.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Linq;
+
 public class GenericEnemy : Character
 {
     protected bool moved = false;
+    protected bool triedTargeting = false;
+    protected bool waiting = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,26 +22,118 @@ public class GenericEnemy : Character
     {
         if(turn) {
             if(!moved) {
-                moved = true;
-                move.BeginTurn();
+                if (!move.moving)
+                {
+                    if (!waiting)
+                    {
+                        move.BeginTurn();
+                        waiting = true;
+                    }
+                    else
+                    {
+                        moved = true;
+                        waiting = false;
+                    }
+                }
             }
         }
-        //add any other bools for if it took other actions here so it will end its turn if all the bools are true
+        
         if(moved) {
-            Debug.Log("Problem?");
-            TurnManager.EndTurn();
+            // check for attack
+            if (!triedTargeting)
+            {
+                if (!waiting)
+                {
+                    AcquireTarget(1, "Player", () => BasicAttackTargetingFunction());
+                    triedTargeting = true;
+                }
+            }
+            else
+            {
+                // target selection is done
+                if (!waiting)
+                {
+                    // valid target found
+                    if (target)
+                    {
+                        // Debug.Log("Target Found");
+                        BasicAttack();
+                    }
+                    
+                    TurnManager.EndTurn();
+                }
+            }
         }
+    }
+
+    virtual protected Collider[] BasicAttackTargetingFunction()
+    {
+        Vector3 halfExtents = new Vector3(1.2f, 4, 1.2f);
+        return Physics.OverlapBox(transform.position, halfExtents, Quaternion.Euler(0, 45, 0));
+    }
+
+    protected IEnumerator WaitAndCleanUpTargeting(Collider[] colliders, float waitTime)
+    {
+        waiting = true;
+        yield return new WaitForSeconds(waitTime);
+
+        // reset tiles
+        foreach (Collider item in colliders)
+        {
+            Tile tile = item.GetComponent<Tile>();
+            if (tile != null)
+            {
+                tile.Reset();
+            }
+        }
+
+        waiting = false;
+    }
+    public delegate Collider[] TargetingFunction();
+    public void AcquireTarget(int range, string desiredTarget, TargetingFunction targetingFunction)
+    {
+        Collider[] colliders = targetingFunction();
+
+        foreach (Collider item in colliders)
+        {
+            Tile tile = item.GetComponent<Tile>();
+            if (tile != null)
+            {
+                tile.attackable = true;
+            }
+        }
+
+        // select target
+        foreach (Collider item in colliders)
+        {
+            Character character = item.GetComponent<Character>();
+            if (character != null)
+            {
+                if (character.CompareTag(desiredTarget))
+                {
+                    target = character;
+                    // color as target
+                    Tile tile = move.GetTargetTile(target.gameObject);
+                    tile.target = true;
+                }
+            }
+        }
+
+        // launch wait and select target coroutine
+        StartCoroutine(WaitAndCleanUpTargeting(colliders, 0.6f));
     }
 
     public override void BeginTurn(){
         moved = false;
+        target = null;
         if (!turn) {turn = true;}
     }
 
     public override void EndTurn()
     {
-        if (moved)
-            moved = false;
+        moved = false;
+        waiting = false;
+        triedTargeting = false;
 
         base.EndTurn();
     }
